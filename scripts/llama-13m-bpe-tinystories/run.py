88dbb8ccf8e6fc09541b7cfc6a4f89615fc4bcc7
baddddddddd <eluxeofficial@gmail.com>
2025-08-16 20:10:11 +0800

    Create scripts for llama-13m-bpe-tinystories

---
+++ scripts/llama-13m-bpe-tinystories/run.py
@@ -0,0 +1,27 @@
+import argparse
+
+import torchinfo
+
+from src.models.llama import LlamaConfig, LlamaModel
+from src.utils import TextGenStdoutStreamer
+from src.tokenizers import TinyStoriesBpe8kTokenizer
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--checkpoint", type=str, required=True, help="Path to the checkpoint folder"
+)
+
+args = parser.parse_args()
+
+tokenizer = TinyStoriesBpe8kTokenizer()
+model = LlamaModel.from_pretrained(args.checkpoint)
+
+streamer = TextGenStdoutStreamer(tokenizer)
+
+torchinfo.summary(model)
+while True:
+    prompt = input("\n>>> ")
+
+    input_ids = tokenizer.encode(prompt, return_tensors="pt")[0, :-1]
+    output = model.generate(input_ids, streamer=streamer)
---
+++ scripts/llama-13m-bpe-tinystories/train.py
@@ -0,0 +1,55 @@
+import torchinfo
+
+from src.datasets import TinyStoriesDataset
+from src.models.llama import LlamaConfig, LlamaModel
+from src.tokenizers import TinyStoriesBpe8kTokenizer
+from src.trainers import CausalLmTrainer, TrainingConfig
+
+
+SEQ_LEN = 256
+D_MODEL = 384
+N_LAYERS = 6
+
+tokenizer = TinyStoriesBpe8kTokenizer(
+    max_length=SEQ_LEN,
+    padding=True,
+    return_overflowing_tokens=True,
+)
+
+model_config = LlamaConfig(
+    vocab_size=tokenizer.get_vocab_size(),
+    n_ctx=SEQ_LEN,
+    d_model=D_MODEL,
+    n_layers=N_LAYERS,
+    n_heads=D_MODEL // 64,
+    bos_token_id=None,
+    eos_token_id=tokenizer.eos_token_id,
+)
+
+training_config = TrainingConfig(
+    output_dir="./checkpoints/llama-13m-bpe-tinystories",
+    num_train_epochs=100,
+    learning_rate=5e-3,
+    min_lr=5e-4,
+    warmup_steps=500,
+    total_steps=25000,
+    betas=(0.9, 0.95),
+    weight_decay=0.01,
+    label_smoothing=0.0,
+    train_batch_size=128,
+    save_steps=100,
+)
+
+model = LlamaModel(model_config)
+torchinfo.summary(model)
+
+dataset = TinyStoriesDataset(split="train", tokenizer=tokenizer)
+
+trainer = CausalLmTrainer(
+    model=model,
+    tokenizer=tokenizer,
+    args=training_config,
+    train_dataset=dataset,
+)
+
+trainer.train(resume_from_checkpoint=True)
