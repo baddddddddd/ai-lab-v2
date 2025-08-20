3853d59378c97096bbf60048bad3e96685826f26
baddddddddd <eluxeofficial@gmail.com>
2025-08-20 11:39:38 +0800

    Specifify cuda in amp scaler

--- src/trainers/trainer.py
+++ src/trainers/trainer.py
@@ -84,7 +84,7 @@ class Trainer:
         dataset_size = len(self.train_dataset)
         counter_width = len(str(dataset_size))
 
-        scaler = torch.amp.GradScaler()
+        scaler = torch.amp.GradScaler(device="cuda")
 
         self.model.train()
         for epoch in range(start_epoch, self.args.num_train_epochs):
@@ -98,7 +98,7 @@ class Trainer:
 
                 inputs = self._prepare_inputs(inputs)
 
-                with torch.amp.autocast():
+                with torch.amp.autocast(device_type="cuda"):
                     output = self.model(**inputs)
                     loss = output.loss
 
