6e86f6edd8592954f86dd21e8a3b6bc193ac9d58
baddddddddd <eluxeofficial@gmail.com>
2025-08-20 10:32:39 +0800

    Remove BaseTrainer

--- src/trainers/__init__.py
+++ src/trainers/__init__.py
@@ -1,3 +1,2 @@
-from .base_trainer import BaseTrainer
-from .causal_lm_trainer import CausalLmTrainer
+from .trainer import Trainer
 from .training_config import TrainingConfig
--- src/trainers/base_trainer.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-class BaseTrainer:
-    def train(self, resume_from_checkpoint: os.PathLike[str] | bool = False):
-        raise NotImplementedError("train() method is not implemented")
