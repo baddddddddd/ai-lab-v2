860c8fb3bf08769a501b03f749316e7617884064
baddddddddd <eluxeofficial@gmail.com>
2025-08-20 08:44:50 +0800

    Add abstract method in BaseDataset classes for getting dataloader

--- src/datasets/base_dataset.py
+++ src/datasets/base_dataset.py
@@ -10,7 +10,13 @@ class BaseDataset(Dataset, Sized):
     def __getitem__(self, idx: int):
         raise NotImplementedError("__getitem__() method is not implemented")
 
+    def get_dataloader(self, batch_size: int, shuffle: bool = False):
+        raise NotImplementedError("get_dataloader() method is not implemented")
+
 
 class BaseStreamingDataset(IterableDataset):
     def __iter__(self):
         raise NotImplementedError("__iter__() method is not implemented")
+
+    def get_dataloader(self, batch_size: int):
+        raise NotImplementedError("get_dataloader() method is not implemented")
