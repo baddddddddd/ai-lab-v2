bd30088827ce05fea71453834d114f036da76a56
baddddddddd <eluxeofficial@gmail.com>
2025-08-22 09:47:31 +0800

    Add use_cache option for CausalLmGenerationMixin

--- src/utils/generation/causal_lm_generation_mixin.py
+++ src/utils/generation/causal_lm_generation_mixin.py
@@ -12,55 +12,97 @@ class CausalLmGenerationMixin:
         max_new_tokens: int = 1024,
         temperature: float = 0.8,
         top_p: float = 0.9,
+        use_cache: bool = True,
         streamer: BaseStreamer | None = None,
     ):
-        generated = input_ids.clone().tolist()
-        if streamer is not None:
-            streamer.put(input_ids)
+        if use_cache:
+            generated = input_ids.clone().tolist()
+            if streamer is not None:
+                streamer.put(input_ids)
 
-        model_input = torch.tensor(
-            [generated],
-            dtype=torch.long,
-            device=input_ids.device,
-        )
-        past_key_values = None
-        start_pos = 0
-        for _ in range(max_new_tokens):
-            output = self.forward(
-                model_input,
-                past_key_values=past_key_values,
-                start_pos=start_pos,
-                use_cache=True,
-            )
-            logits = output.logits[0, -1]
-            past_key_values = output.past_key_values
-            start_pos = len(generated)
-
-            if temperature > 0.0:
-                logits /= temperature
-                if top_p > 0.0:
-                    next_token = top_p_sample(logits, p=top_p).item()
-                else:
-                    probs = F.softmax(logits, dim=-1)
-                    next_token = torch.multinomial(probs, num_samples=1).item()
-            else:
-                next_token = torch.argmax(logits).item()
-
-            generated.append(next_token)
             model_input = torch.tensor(
-                [[next_token]], dtype=torch.long, device=input_ids.device
+                [generated],
+                dtype=torch.long,
+                device=input_ids.device,
             )
+            past_key_values = None
+            start_pos = 0
+            for _ in range(max_new_tokens):
+                output = self.forward(
+                    model_input,
+                    past_key_values=past_key_values,
+                    start_pos=start_pos,
+                    use_cache=True,
+                )
+                logits = output.logits[0, -1]
+                past_key_values = output.past_key_values
+                start_pos = len(generated)
+
+                if temperature > 0.0:
+                    logits /= temperature
+                    if top_p > 0.0:
+                        next_token = top_p_sample(logits, p=top_p).item()
+                    else:
+                        probs = F.softmax(logits, dim=-1)
+                        next_token = torch.multinomial(probs, num_samples=1).item()
+                else:
+                    next_token = torch.argmax(logits).item()
+
+                generated.append(next_token)
+                model_input = torch.tensor(
+                    [[next_token]], dtype=torch.long, device=input_ids.device
+                )
+
+                if streamer is not None:
+                    streamer.put(torch.tensor([next_token], device=input_ids.device))
+
+                if (
+                    self.config.eos_token_id is not None
+                    and next_token == self.config.eos_token_id
+                ):
+                    break
 
             if streamer is not None:
-                streamer.put(torch.tensor([next_token], device=input_ids.device))
+                streamer.end()
 
-            if (
-                self.config.eos_token_id is not None
-                and next_token == self.config.eos_token_id
-            ):
-                break
+            return torch.tensor(generated, device=input_ids.device)
+        else:
+            # old sliding window implementation
+            from collections import deque
 
-        if streamer is not None:
-            streamer.end()
+            generated = deque(input_ids.clone().tolist(), maxlen=self.config.n_ctx)
+            if streamer is not None:
+                streamer.put(input_ids)
 
-        return torch.tensor(generated, device=input_ids.device)
+            for _ in range(max_new_tokens):
+                model_input = torch.tensor(
+                    [generated], dtype=torch.long, device=input_ids.device
+                )
+                logits = self.forward(model_input).logits
+                logits = logits[0, -1]
+
+                if temperature > 0.0:
+                    logits /= temperature
+                    if top_p > 0.0:
+                        next_token = top_p_sample(logits, p=top_p)
+                    else:
+                        probs = F.softmax(logits, dim=-1)
+                        next_token = torch.multinomial(probs, num_samples=1).item()
+                else:
+                    next_token = torch.argmax(logits).item()
+
+                generated.append(next_token)
+
+                if streamer is not None:
+                    streamer.put(torch.tensor([next_token], device=input_ids.device))
+
+                if (
+                    self.config.eos_token_id is not None
+                    and next_token == self.config.eos_token_id
+                ):
+                    break
+
+            if streamer is not None:
+                streamer.end()
+
+            return torch.tensor(generated, device=input_ids.device)
